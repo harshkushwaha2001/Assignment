c85e28431a0815678f7d4a5e3f88aa7d6a762a3f
harshkushwaha2001 <harshkushwaha32001@gmail.com>
2025-04-25 15:23:17 +0530

    uncommented the codes that are used for individual  testing

--- Django_Signals/app1/signals.py
+++ Django_Signals/app1/signals.py
@@ -3,8 +3,8 @@ from django.db.models.signals import post_save
 from .models import User
 from django.dispatch import receiver
 
-# @receiver(post_save, sender=User)
-# def user_post_save(sender, instance, created, **kwargs):
-#     print("Signal handler started.")
-#     time.sleep(5)  # making it sleep for 5 seconds
-#     print("Signal handler completed.")
+@receiver(post_save, sender=User)
+def user_post_save(sender, instance, created, **kwargs):
+    print("Signal handler started.")
+    time.sleep(5)  # making it sleep for 5 seconds
+    print("Signal handler completed.")
--- Django_Signals/app2/tests.py
+++ Django_Signals/app2/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .models import User
 import threading
 
-# class ThreadCheckTest(TestCase):
-#     def test_signal_runs_in_same_thread(self):
-#         print(f"[Caller] Caller Thread ID: {threading.get_ident()}")
-#         User.objects.create(username='Harsh Kushwaha',email='harshkushwaha32001@gmail.com',address='earth')
+class ThreadCheckTest(TestCase):
+    def test_signal_runs_in_same_thread(self):
+        print(f"[Caller] Caller Thread ID: {threading.get_ident()}")
+        User.objects.create(username='Harsh Kushwaha',email='harshkushwaha32001@gmail.com',address='earth')
